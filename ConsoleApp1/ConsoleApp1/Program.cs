ff54c13435f9688241ea026b2853eff506cc5cec
Marian Cristian Serban <marian.serban109@gmail.com>
2020-02-04 23:44:19 +0200

    added project files

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            AllTableEntity<Obj>().Where(x => x.RowKey == "1");
+
+
+            AllTableEntity<Obj>().Where(x => x.PartitionKey == "1");
+        }
+
+
+
+        public static IQueryable<T> AllTableEntity<T>() where T : ITableEntity, new()
+        {
+            return null;
+        }
+    }
+
+    public class Obj : TableEntity
+    {
+        public string PartitionKey { get; set; }
+        public string RowKey { get; set; }
+    }
+}
